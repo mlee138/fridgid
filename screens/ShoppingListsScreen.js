5c5a49bbd89854f90e336df735d15b7b41359730
mlee138 <mlee138@binghamton.edu>
2019-11-14 16:13:27 -0500

    changed header and added 'send to fridge' button

--- screens/ShoppingListsScreen.js
+++ screens/ShoppingListsScreen.js
@@ -1,6 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
 import React, { Component } from 'react';
 import {
+  Alert,
   AsyncStorage,
   FlatList,
   Platform,
@@ -10,10 +11,7 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import { ListItem, CheckBox } from 'react-native-elements';
-import { MonoText } from '../components/StyledText';
-
-
+import { Button, CheckBox, Icon, Tooltip } from 'react-native-elements';
 
 export default class ShoppingListsScreen extends Component {
   constructor(props){
@@ -26,20 +24,53 @@ export default class ShoppingListsScreen extends Component {
 
   componentDidMount = () => {
     AsyncStorage.getItem('list', (error,result) => {
-      this.setState({ data: JSON.parse(result)}, function(){console.log('success!')})
+      this.setState({ data: this.state.data.concat(JSON.parse(result))}, function(){console.log('data loaded!')})
     });
   }
 
-  setName = () => {
+  storeList = (newList) => {
+    AsyncStorage.setItem('list', JSON.stringify(newList))
+    .then(json => console.log('data saved!'))
+    .catch(error => console.log('error saving!'));
+  }
+
+  handleAdd = () => {
+    if(!this.checkDuplicate()){
+      this.addItem();
+    } else{
+        Alert.alert(
+          'No Duplicates Allowed',
+          'Item has not been added',
+          [
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+              style: 'cancel',
+            },
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+          ],
+        );  
+    }
+  }
+
+  checkDuplicate = () => {
+    var oldData = this.state.data;
+    for(i=0; i<oldData.length; i++){
+      if(oldData[i].name === this.state.text){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addItem = () => {
     var newElement = {
       name: this.state.text,
       checked: false,
     }
     var newData = [...this.state.data, newElement];
     this.setState({ data: newData});
-    AsyncStorage.setItem('list', JSON.stringify(newData))
-      .then(json => console.log('success!'))
-      .catch(error => console.log('error!'));
+    this.storeList(newData);
   }
 
   checkItem = (item) => {
@@ -51,23 +82,68 @@ export default class ShoppingListsScreen extends Component {
         newObj['checked'] = obj.checked)
       return newObj;
     });
-    
+    this.storeList(newArray);
     this.setState({data: newArray});
+  }
+
+  deleteItem = (item) => {
+    const newData = this.state.data;
+    for(i = 0; i < newData.length; i++){
+      if(newData[i].name === item){
+        newData.splice(i, 1);
+        console.log(newData);
+        this.storeList(newData);
+        this.setState({data: newData});
+      }
+    }
+
+  }
+
+  fridgeAlert = () => {
+    Alert.alert(
+      'Send Checked items to Fridge?',
+      'Any item sent will be placed in the fridge and removed from your shopping list.',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => this.sendToFridge()},
+      ],
+    ); 
+  }
+
+  sendToFridge = () => {
+    console.log("OK: send to fridge");
   }
 
   render(){
     return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Shopping List</Text>
+      <ScrollView style={styles.container}>
+        <View style={styles.inputs}>
+          <TextInput
+            value={this.state.text}
+            maxLength={20}
+            style={styles.textInput} 
+            onChangeText = {(value) => this.setState({text: value})}
+            onSubmitEditing = {this.handleAdd}
+            onFocus = {() => this.setState({text: ''})}
+            placeholder = 'Add item'/>
+          <Text style={styles.divider}/>
+          <Button
+            type="clear"
+            onPress={ this.fridgeAlert }
+            icon={
+              <Icon 
+                name='select1' 
+                type='antdesign'
+                color='#00aced'
+              />
+            }
+            iconRight
+          />
         </View>
-        <TextInput
-          value={this.state.text}
-          style={styles.textInput} 
-          onChangeText = {(value) => this.setState({text: value})}
-          onSubmitEditing = {this.setName}
-          onFocus = {() => this.setState({text: ''})}
-          placeholder = 'Add item'/>
         <FlatList
           data={this.state.data}
           renderItem={({ item }) => (
@@ -75,43 +151,50 @@ export default class ShoppingListsScreen extends Component {
               title={item.name}
               checked={item.checked}
               onPress={() => this.checkItem(item.name)}
+              onLongPress={() => this.deleteItem(item.name)}
             />
           )}
           keyExtractor={item => item.name}
         />
-      </View>
+      </ScrollView>
     );
   }
 }
 
 ShoppingListsScreen.navigationOptions = {
-  header: null,
+  title: "Shopping List",
+  headerTitleStyle: {
+    flexGrow: 2,
+  }
+  
 };
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 15,
     backgroundColor: '#fff',
   },
-  header: {
-    display: 'flex',
+  inputs: {
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
-    borderBottomColor: 'grey',
-    borderBottomWidth: 2,
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
   },
   textInput: {
+    flexGrow:2,
     borderRadius: 20,
     backgroundColor: '#ececec',
     padding: 3,
     paddingLeft: 15,
     paddingRight: 15,
-    margin: 5,
+    marginRight: 8,
+  },
+  divider: {
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
