d83b0d8344cd2df730a53b7d230bda0f7242c7e5
mlee138 <mlee138@binghamton.edu>
2019-12-05 18:46:07 -0500

    added fridgeitem add, update, and delete functionality

--- screens/FridgeScreen.js
+++ screens/FridgeScreen.js
@@ -12,7 +12,8 @@ export default class FridgeScreen extends Component  {
     this.state = {
       data: [],
       update: true,
-      addVisible: false,
+      modalVisible: false,
+      modal: 'Add',
       showPicker: false,
       item: '',
       date:"05-15-2018",
@@ -47,10 +48,51 @@ export default class FridgeScreen extends Component  {
     .catch(error => console.log('error saving fridge!'));
   }
 
-  addFridgeItem = () => {
-    if(this.state.item){
+  handleOpenModal = (modalType, itemName, itemDate) => {
+    switch(modalType){
+      case 'Add':
+          this.setState({ item: ''});
+          this.setState({ date: this.today});
+        break;
+      case 'Update':
+          this.setState({ item: itemName });
+          this.setState({ oldItem: itemName });
+          this.setState({ date: itemDate });
+        break;
+      default:
+        console.log("handleOpenModal Error probably");
+        break;
+    }
+    this.setState({ modal: modalType});
+    this.setModalVisible(!this.state.modalVisible);
+  }
 
-    } else {
+  handleFridgeSubmit = () => {
+    switch(this.state.modal){ 
+      case "Add":
+        this.addFridgeItem();
+        break;
+      case "Update":
+        this.updateFridgeItem();
+        break;
+      default:
+        console.log("Well this is awkward");
+        break;
+    }
+  }
+
+  noDuplicates = () => {
+    var noDuplicates = true;
+    this.state.data.forEach(element => {
+      if(element.name == this.state.item){ 
+        noDuplicates = false;
+      }
+    })
+    return noDuplicates;
+  }
+
+  addFridgeItem = () => {
+    if(this.state.item && this.noDuplicates()){
       var newElement = {
         name: this.state.item,
         date: this.state.date,
@@ -59,13 +101,54 @@ export default class FridgeScreen extends Component  {
       this.setState({ data: newData});
       this.storeFridge(newData);
 
-      this.setModalVisible(!this.state.addVisible);
+      this.setModalVisible(!this.state.modalVisible);
+    } else {
+      Alert.alert(
+        'User Error',
+        'The item entered is either a duplicate or has no name',
+        [ 
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+      );
     }
-    
+  }
+
+  updateFridgeItem = () => {
+    var oldName = this.state.oldItem;
+    var newName = this.state.item;
+    var newDate = this.state.date;
+
+    var newArray = this.state.data.map(function(obj){
+      var newObj = {};
+      if(obj.name === oldName){
+        newObj = {
+          name: newName,
+          date: newDate,
+        }
+        return newObj;
+      } else{
+        return obj;
+      }
+    })
+    this.storeFridge(newArray);
+    this.setState({ data: newArray});
+    this.setModalVisible(!this.state.modalVisible);
+  }
+
+  deleteItem = (itemName) => {
+    const newData = this.state.data;
+    for(i = 0; i < newData.length; i++){
+      if(newData[i].name === itemName){
+        newData.splice(i, 1);
+        this.storeFridge(newData);
+        this.setState({data: newData}); 
+      }
+    }
+    if(newData.length === 0){ this.setState({data: []}); }
   }
 
   setModalVisible = (visible) => {
-    this.setState({ addVisible: visible });
+    this.setState({ modalVisible: visible });
   }
 
   render(){    
@@ -74,55 +157,58 @@ export default class FridgeScreen extends Component  {
         <View>
           <Button
             title="Add Item"
-            onPress={() => {
-              this.setModalVisible(!this.state.addVisible)
-            }}/>
+            onPress={() => this.handleOpenModal('Add')}/>
         </View>
         <Modal
           animationType="slide"
           transparent={false}
-          visible={this.state.addVisible}
+          visible={this.state.modalVisible}
           onRequestClose={() => {
-            this.setModalVisible(!this.state.addVisible)
+            this.setModalVisible(!this.state.modalVisible)
           }}>
-          <Text>Add Fridge Item</Text>
+          <Text>{this.state.modal} Fridge Item</Text>
           <View style={styles.datepicker}>
             <Input
               placeholder="Item name"
               label="Name"
               value={this.state.item}
-              onChangeText= { text => this.setState({ item: text})}/>
-            <DatePicker
-              style={{width: 200}}
-              date={this.state.date} //initial date from state
-              mode="date" //The enum of date, datetime and time
-              placeholder="select date"
-              format="MM-DD-YYYY"
-              minDate="01-01-2016"
-              maxDate={this.today}
-              confirmBtnText="Confirm"
-              cancelBtnText="Cancel"
-              customStyles={{
-                dateIcon: {
-                  position: 'absolute',
-                  left: 0,
-                  top: 4,
-                  marginLeft: 0
-                },
-                dateInput: {
-                  marginLeft: 36
-                }
-              }}
-              onDateChange={(date) => {this.setState({date: date})}}
-            />
-              <View style={styles.modalBtns}>
+              onChangeText= { text => this.setState({ item: text})}
+              autoFocus/>
+              <View>
+                <Text>Date put in fridge:</Text>
+                <DatePicker
+                  style={{width: 200}}
+                  date={this.state.date} //initial date from state
+                  mode="date" //The enum of date, datetime and time
+                  placeholder="select date"
+                  format="MM-DD-YYYY"
+                  minDate="01-01-2016"
+                  maxDate={this.today}
+                  confirmBtnText="Confirm"
+                  cancelBtnText="Cancel"
+                  customStyles={{
+                    dateIcon: {
+                      position: 'absolute',
+                      left: 0,
+                      top: 4,
+                      marginLeft: 0
+                    },
+                    dateInput: {
+                      marginLeft: 36
+                    }
+                  }}
+                  onDateChange={(date) => {this.setState({date: date})}}
+                />
+              </View>
+              
+              <View>
                 <Button
                 title="cancel"
-                onPress={() => this.setModalVisible(!this.state.addVisible)}
+                onPress={() => this.setModalVisible(!this.state.modalVisible)}
                 />
                 <Button
-                title="confirm"
-                onPress={() => this.addFridgeItem}
+                title={this.state.modal}
+                onPress={ this.handleFridgeSubmit}
                 />
               </View>
             </View>            
@@ -133,6 +219,8 @@ export default class FridgeScreen extends Component  {
             <ListItem
               title={item.name}
               bottomDivider
+              onPress={() => this.handleOpenModal('Update', item.name, item.date) }
+              onLongPress={() => this.deleteItem(item.name)}
             />
           )}
           keyExtractor={item => item.name}
